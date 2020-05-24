07d487d195f61400cf2dbe714ce8e631911d9e0a
mayarasantos927 <mayara.santos@nextpage.com.br>
2020-05-24 13:13:32 -0300

    Mayara de Lima Santos - 5º ADS - Noite

---
+++ app/src/main/java/com/example/fatec_ipi_noite_chat_com_firebase/Categoria.java
@@ -0,0 +1,15 @@
+package com.example.fatec_ipi_noite_chat_com_firebase;
+
+public class Categoria {
+    private String categoria;
+
+    public Categoria() {}
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
